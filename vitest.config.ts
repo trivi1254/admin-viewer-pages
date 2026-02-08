b98710dd32ac0411d95e684767e9a1277144faa0
[trivi1254] <[johnmichaelloor@gmail.com]>
2026-02-08 13:30:30 -0500

    jabdlqkjdlq

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,19 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
+
 
 export default defineConfig({
   base: "/admin-viewer-pages/",
-
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), componentTagger()],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
